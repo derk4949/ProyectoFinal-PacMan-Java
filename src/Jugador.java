9f857e0542beb55af0ebb1223d5263b48f459dfa
PC <leonarxsgameryt@gmail.com>
2026-06-25 16:54:13 -0500

    PROCESO

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,4 +1,63 @@
+import java.util.Scanner;
+
 public class Jugador {
+    public String nombre;
+    public int fila;
+    public int columnna;
+    public int puntaje;
+    public int salud;
+    public int velocidad;
+    public boolean poderActivo;
+
+    //Nombre
+    public Jugador(String nombreInicial) {
+        this.nombre = _nombreInicial;
+    }
+    //Posicion
+    public Jugador(int filaInicial, int columnaInicial) {
+        this.fila = filaInicial;
+        this.columnna = columnaInicial;
+    }
+    //Puntaje y velocidad
+    public Jugador (int puntaje,int salud,int velocidad){
+        this.puntaje=puntaje;
+        this.salud=salud;
+        this.velocidad = velocidad;
+
+    }
+    //Poder con velocidad
+    public Jugador (boolean poderActivo,int velocidad) {
+        this.poderActivo=poderActivo;
+        this.velocidad = velocidad;
+
+    }
+    public static void main(String[]args){
+
+    }
 
 
+    public void mover() {
+    }
+
+    public void recogerPunto() {
+        this.puntaje++;
+    }
+
+    public void recibirDano() {
+        this.salud -= 20;
+    }
+
+    public void usarPoder() {
+    }
+
+    public void estaVivo() {
+    }
+
+    public void mostrarEstado() {
+        System.out.println("--- ESTADO DE: " + this.nombre + " ---");
+        System.out.println("Vida: " + this.salud + "%");
+        System.out.println("Puntaje: " + this.puntaje);
+        System.out.println("Posición: [" + this.fila + ", " + this.columnna + "]");
+        System.out.println("-------------------------");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola mundio");
-        System.out.println("hola mundio");
-        System.out.println("hola mundio");
+        Scanner lector=new Scanner(System.in);
+        System.out.println("Ingrese su nombre de usuario");
+        String nombreJugador = lector.nextLine();
+        Jugador nomJugador = new Jugador(nombreJugador);
 
-
+        Jugador Posicion = new Jugador(1,1);
+        Jugador Puntajeyvelocidad = new Jugador(1,1,1);
+        Jugador PoderyVelocidad =
+        jugadorUnico.mostrarEstado();
     }
 }
