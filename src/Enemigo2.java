eb0992582277ec0d21b286adc3072083232aceac
Derk4949 <xcal4949@hotmail.com>
2026-06-26 10:08:00 -0500

    constructor clase enemigo2 (aleatorio)

--- src/Enemigo2.java
+++ src/Enemigo2.java
@@ -1,9 +1,25 @@
+import java.util.Random;
+
 public class Enemigo2 {
     public String tipo;
     public int fila;
     public int columna;
     public int danio;
     public boolean activo;
+    public Random random;
+
+    public Enemigo2(int fila, int columna) {
+        this.tipo    = "Aleatorio";
+        this.fila    = fila;
+        this.columna = columna;
+        this.danio   = 1;
+        this.activo  = true;
+        this.random  = new Random();
+    }
+
+
+
+
 
 
     public void mover() {
@@ -11,6 +27,7 @@ public class Enemigo2 {
     }
 
     public void atacar(){
+        System.out.println("El enemigote choco!");
 
     }
     public boolean verificarColision(){
