9495fe87bf597fcb5d5bb4e8a3c5c8a4e7eda935
z1x2522x1z-maker <z1x2522x1z@gmail.com>
2026-06-29 13:04:37 -0500

    Clase completa - aunq tengo algunas dudas.

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,17 +1,28 @@
 public class Tablero {
+    //Atributos basicos de la clase
     public int filas;
     public int columnas;
     public char [][] matriz;
+    //Arreglos de objetos
+    public Muro [] muros;
+    public Poder [] poderes;
+    private Punto[] puntos;
 
-    public Tablero (int _filas, int _columnas) {
+    //Constructor
+    public Tablero (int _filas, int _columnas, int _cantMuros, int _cantPuntos, int _cantPoderes) {
         filas = _filas;
         columnas = _columnas;
         matriz = new char[filas][columnas];
+        muros = new Muro[_cantMuros];
+        poderes = new Poder[_cantPoderes];
+        this.puntos = new Punto[_cantPuntos];
+
     }
+
     public void generarTablero () {
         //Tablero();
         for (int i=0 ; i<matriz.length ; i++){
-            for (int j = 0; j<matriz[0].length ; j++){
+            for (int j = 0; j<matriz[i].length ; j++){
                 if (i==0 || i==filas-1 || j==0 || j==columnas-1){
                     matriz[i][j] = '#';
                 }else{
@@ -21,6 +32,7 @@ public class Tablero {
         }
         System.out.println();
     }
+
     public void mostrarTablero () {
         //Tablero();
         for (int i=0 ; i<matriz.length ; i++){
