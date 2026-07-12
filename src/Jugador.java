b0dd7cd1966106070c527ddb872acdba2a7696cb
Derk4949 <xcal4949@hotmail.com>
2026-07-12 00:41:24 -0500

    Pruebas impresiones en tablero, Enemigo 2 y jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,27 +1,6 @@
 import java.util.Scanner;
 
 public class Jugador {
-    private int fila;
-    private int columna;
-
-
-    public Jugador() {
-
-    }
-
-
-
-    public void recibirDanio(int danio){
-
-    }
-
-    public int getFila(int fila){
-        return this.fila = fila;
-    }
-
-    public int getColumna(int columna){
-        return this.fila = fila;
-    }
 
     //1. ATRIBUTOS
     private String nombre;
