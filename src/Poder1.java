0f4a3fc93931fc0246d9957659b4286a0171a545
PC <leonarxsgameryt@gmail.com>
2026-07-18 22:55:56 -0500

    .

--- src/Poder1.java
+++ src/Poder1.java
@@ -1,12 +1,3 @@
-// Poder1: representa el poder de velocidad. Hereda de Poder todos los
-// atributos comunes (tipo, duracion, fila, columna, recogido).
-public class Poder1 extends Poder {
+public class Poder1  {
 
-    // Constructor: solo recibe la posicion. El tipo y la duracion de este
-    // poder siempre son los mismos, asi que se los pasamos fijos a Poder.
-    public Poder1(int fila, int columna) {
-        super("Velocidad", 5, fila, columna);
-    }
-
-    // Los métodos activadores y la descripción se añadirán en el siguiente commit
 }
