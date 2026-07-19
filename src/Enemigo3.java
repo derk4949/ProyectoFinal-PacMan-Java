937b1d9bf9c209e29d07dd282b76f29b9e95faed
z1x2522x1z-maker <z1x2522x1z@gmail.com>
2026-07-18 23:18:06 -0500

    Se implementa el movimiento aleatorio del fantasma.

--- src/Enemigo3.java
+++ src/Enemigo3.java
@@ -39,5 +39,62 @@ public class Enemigo3 extends Enemigo {
                 && getColumna() == jugador.getColumna()) {
             return;
         }
+
+        // Elige una de las cuatro direcciones posibles.
+        int direccion = random.nextInt(4);
+
+        // Prueba cada direccion hasta encontrar un movimiento valido.
+        for (int intento = 0; intento < 4; intento++) {
+
+            int nuevaFila = getFila();
+            int nuevaColumna = getColumna();
+
+            switch (direccion) {
+
+                case 0:
+                    // Arriba
+                    nuevaFila = getFila() - 1;
+                    if (nuevaFila == 0) {
+                        nuevaFila = tablero.getFilas() - 2;
+                    }
+                    break;
+
+                case 1:
+                    // Abajo
+                    nuevaFila = getFila() + 1;
+                    if (nuevaFila == tablero.getFilas() - 1) {
+                        nuevaFila = 1;
+                    }
+                    break;
+
+                case 2:
+                    // Izquierda
+                    nuevaColumna = getColumna() - 1;
+                    if (nuevaColumna == 0) {
+                        nuevaColumna = tablero.getColumnas() - 2;
+                    }
+                    break;
+
+                case 3:
+                    // Derecha
+                    nuevaColumna = getColumna() + 1;
+                    if (nuevaColumna == tablero.getColumnas() - 1) {
+                        nuevaColumna = 1;
+                    }
+                    break;
+            }
+
+            // El fantasma puede avanzar aunque la casilla tenga un muro.
+            if (intentarMoverIgnorandoMuros(nuevaFila, nuevaColumna, tablero)) {
+                return;
+            }
+
+            // Si no puede avanzar, prueba la siguiente direccion.
+            direccion++;
+
+            if (direccion == 4) {
+                direccion = 0;
+            }
+        }// Si ninguna de las 4 direcciones fue valida, el Fantasma permanece en su posicion actual durante este turno.
     }
 }
