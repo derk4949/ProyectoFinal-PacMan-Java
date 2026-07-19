e9721ecfe3a709c95687b4ebb8829f095c5698ef
PC <leonarxsgameryt@gmail.com>
2026-07-18 20:35:44 -0500

    Inicializada clase ControlEnemigos con atributos y validaciones de estado de grupo

--- src/Control_de_Enemigos.java
+++ src/Control_de_Enemigos.java
@@ -1,3 +1,47 @@
-public class Control_de_Enemigos {
+// Clase ControlEnemigos: se encarga de administrar a TODOS los enemigos del juego como grupo.
+public class ControlEnemigos {
 
+    // Arreglo de enemigos controlados. Es el MISMO arreglo que tiene Tablero.
+    private Enemigo[] enemigos;
+
+    // Jugador contra el que se revisan las colisiones.
+    private Jugador jugador;
+
+    // CONSTRUCTOR
+    public ControlEnemigos(Enemigo[] enemigos, Jugador jugador) {
+        if (enemigos == null) {
+            this.enemigos = new Enemigo[0];
+        } else {
+            this.enemigos = enemigos;
+        }
+        this.jugador = jugador;
+    }
+
+    // CONGELACION
+    private boolean enemigosCongelados() {
+        return jugador != null
+                && jugador.tienePoderActivo()
+                && "Congelacion".equals(jugador.getTipoPoderActivo());
+    }
+
+    // POSICIONES OCUPADAS
+    private boolean posicionOcupadaPorOtro(int indiceActual, int fila, int columna) {
+        for (int i = 0; i < enemigos.length; i++) {
+            Enemigo otro = enemigos[i];
+
+            if (otro == null) continue;
+            if (i == indiceActual) continue;
+            if (!otro.estaActivo()) continue;
+
+            if (otro.getFila() == fila && otro.getColumna() == columna) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // GETTER
+    public Enemigo[] getEnemigos() {
+        return enemigos;
+    }
 }
