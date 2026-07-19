71764ee533acec46c5060b84f0a959334db00e16
PC <leonarxsgameryt@gmail.com>
2026-07-19 13:23:09 -0500

    Introduciendo denuevo la clase por equivocacion de clase correspondiente

--- src/Poder1.java
+++ src/Poder1.java
@@ -1,3 +1,12 @@
-public class Poder1  {
+// Poder1: representa el poder de velocidad. Hereda de Poder todos los
+// atributos comunes (tipo, duracion, fila, columna, recogido
+public class Poder1 extends Poder {
 
+    // Constructor: solo recibe la posicion. El tipo y la duracion de este
+    // poder siempre son los mismos, asi que se los pasamos fijos a Poder.
+    public Poder1(int fila, int columna) {
+        super("Velocidad", 5, fila, columna);
+    }
+
+    // Los metodos activadores y la descripción se añadiran en el siguiente commit
 }
